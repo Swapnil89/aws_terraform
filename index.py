fcdc09862262e750b4bfdecc4e52b97cf8c3f54e
Swapnil Bari <bari.swapnil89@gmail.com>
2021-03-04 14:00:45 +0530

    Latest working lambda code
    Latest working lambda code

--- index.py
+++ index.py
@@ -2,37 +2,120 @@ import os
 os.environ['AWS_DATA_PATH'] = '/opt/'
 
 from itertools import islice
-import boto3
 from datetime import datetime
 import time
 import json
+import boto3
 
 region = os.environ['AWS_REGION']
 destination_bucket = os.environ['S3_BUCKET']
+inbound_sqs_queue = os.environ['INBOUND_SQS_QUEUE']
+outbound_sqs_queue = os.environ['OUTBOUND_SQS_QUEUE']
+destination_folder = "adx/export/"
 
-if not destination_bucket:
-    raise Exception("'S3_BUCKET' environment variable must be defined!")
+# print("Region: {}".format(region))
+# print("destination_folder: {}".format(destination_folder))
+
+dataexchange = boto3.client(
+    service_name='dataexchange',
+    region_name=region
+)
+s3 = boto3.client(
+    service_name='s3',
+    region_name=region
+)
+sqs = boto3.client(
+    service_name='sqs',
+    region_name=region
+)
+
+if not destination_bucket and not inbound_sqs_queue and not outbound_sqs_queue:
+    raise Exception("Environment variables. 'S3_BUCKET': {}, 'INBOUND_SQS_QUEUE': {}, 'OUTBOUND_SQS_QUEUE': {}".format(destination_bucket, inbound_sqs_queue, outbound_sqs_queue))
+
+def create_message(dataset_id, revision_id):
+    # assets_details = dataexchange.list_revision_assets(DataSetId=dataset_id, RevisionId=revision_id)
+    # assets_details = assets_details['Assets']
+    # print('Assets: {}'.format(assets_details))
+    # print('Asset_list: {}'.format(assets_details['Assets']))
+    message = {
+        'dataset_id' : dataset_id,
+        'revision_id': revision_id,
+        "location": "adx/export/" + dataset_id + "/" + revision_id
+    }
+    return json.dumps(message)
 
 def handler(event, context):
-    dataexchange = boto3.client(
-        service_name='dataexchange',
-        region_name=region
-    )
-    s3 = boto3.client(
-        service_name='s3',
-        region_name=region
-    )
-    sqs = boto3.client(
-        service_name='sqs',
-        region_name=region
-    )
-    message = sqs.receive_message(
-        QueueUrl='https://sqs.us-east-1.amazonaws.com/635065826439/adx_sqs_queue'
-    )
-    print("##### Event -- START")
-    print(message)
-    print("##### Event -- END")
+    print("Boto3 version: {}".format(boto3.__version__))
+    print("Event: {}".format(event)) # debug logging
+    if 'InitialInit' in event:
+        dataset_id = event['InitialInit']['data_set_id']
+        revision_ids = [event['InitialInit']['RevisionIds']] 
+        print ("Initial revision retrieved. dataset_id: {}, revision_ids: {}".format(dataset_id, revision_ids))
+
+    else:    
+        for record in event['Records']:
+            body = json.loads(record["body"])
+            # message = json.loads(body['Message'])
+            dataset_id = body['resources'][0]
+            revision_ids = body['detail']['RevisionIds']
+            print("Event from SQS retrieved. dataset_id: {}, revision_id: {}".format(dataset_id, revision_ids))
+            
+        # Used to store the Ids of the Jobs exporting the assets to S3.
+        job_ids = set()
+
+        # iterate all revision ids to get assets
+    for revision_id in revision_ids:
+        # Need set retry on a message if lambda fails 3 times to process msg, a cloudwatch event must be raised to notify
+                 
+        # Start Jobs to export all the assets to S3.
+        # try:
+            ## Need to add revision asset. dataset
+            revision_assets = dataexchange.list_revision_assets(DataSetId=dataset_id, RevisionId=revision_id)
+            # Create the Job which exports assets to S3. 
+            export_job = dataexchange.create_job(
+                Type='EXPORT_REVISIONS_TO_S3',
+                Details={
+                 'ExportRevisionsToS3': {
+                    'DataSetId': dataset_id,
+                # 'Encryption': {
+                #     'KmsKeyArn': 'string',
+                #     'Type': 'aws:kms'|'AES256'
+                # },
+                    'RevisionDestinations': [
+                        { 'Bucket': destination_bucket, 'RevisionId': revision_id, 'KeyPattern': destination_folder + dataset_id + "/" + "${Revision.Id}/${Asset.Name}" }
+                    ]
+                  }
+                }    
+            )
+            # Start the Job and save the JobId.
+            dataexchange.start_job(JobId=export_job['Id'])
+            job_ids.add(export_job['Id'])
+
+        # except InternalServerException as e:
+        #     # Message will be 'in-flight' and will be available for consumption after visibility timeout expires.
+        #     print('Error in processing revision: {}'.format(e.message))   #https://docs.aws.amazon.com/data-exchange/latest/apireference/v1-jobs.html
+            
+    # Iterate until all remaining workflow have reached a terminal state, or an error is found.
+    completed_jobs = set()
+    while job_ids != completed_jobs:
+        for job_id in job_ids:
+            if job_id in completed_jobs:
+                continue
+            get_job_response = dataexchange.get_job(JobId=job_id)
+            if get_job_response['State'] == 'COMPLETED':
+                print ("Job {} completed".format(job_id))
+                completed_jobs.add(job_id)
+                # publish event in SNS Topic
+                message = create_message(dataset_id,revision_ids[0])
+                sqs.send_message(QueueUrl=outbound_sqs_queue, MessageBody=message, MessageGroupId=dataset_id)
+            if get_job_response['State'] == 'ERROR':
+                job_errors = get_job_response['Errors']
+                raise Exception('JobId: {} failed with errors:\n{}'.format(job_id, job_errors))
+            # Sleep to ensure we don't get throttled by the GetJob API.
+            time.sleep(0.2)        
+
     return {
         'statusCode': 200,
         'body': json.dumps('All jobs completed.')
     }
+
